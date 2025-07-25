19d2f59607453f168400dde31f31e923c2b08e3a
angularexample <info@angularexample.com>
2025-07-25 19:26:13 -0400

    add ui-label to app-one

--- apps/app-one/src/app/app.component.ts
+++ apps/app-one/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { UiLabelComponent } from '@angular-mfe-nx/ui';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, UiLabelComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
